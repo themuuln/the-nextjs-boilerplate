110c47df288c40b3bb662616c0302b37da742af8
Themuuln <zerone.offical@gmail.com>
2024-06-27 23:30:38 +0800

    chore: clg replaced with conlg package

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,14 +1,6 @@
-const isDev = true;
-
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const clg = (...args: any[]) => {
-  if (isDev) {
-    console.log(...args);
-  }
-};
